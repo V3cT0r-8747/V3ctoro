20d9ce0b96f83c331440f21a207bdb6dfd73d3c8
Billy <billyzhaodong@gmail.com>
2022-04-02 21:43:28 +0100

    get clientId and guildId

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 //prerequisites
 const { Client, Intents } = require('discord.js'); //gets discord.js
-const { token } = require('./config.json');  //gets token from config.json
+const { token, clientId, guildId } = require('./config.json');  //gets token from config.json
 
+const commands = ['ping', 'server', 'user'];
 // Create a client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 //checks if client is ready to go
 client.once('ready', () => {
-    console.log('Ready to go!');
-    client.user.setActivity('avoiding death by code'); //sets custom status
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setPresence({ activities: [{ name: 'beep-boop, being worked on rn' }], status: 'online' }); //sets activity/status
 });
+
 // Login to Discord with your client's token
 client.login(token);
