3ee27f383e74791c9c7fc4d139d2a54b5cacf693
Billy <billyzhaodong@gmail.com>
2022-04-02 17:53:35 +0100

    Added "playing" custom status

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 //checks if client is ready to go
 client.once('ready', () => {
     console.log('Ready to go!');
-    client.user.setActivity('avoiding death by code');
+    client.user.setActivity('avoiding death by code'); //sets custom status
 });
 // Login to Discord with your client's token
 client.login(token);
