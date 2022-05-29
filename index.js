06bac4ea9822c165d754095de46ec5f27e179938
Billy <billyzhaodong@gmail.com>
2022-05-29 17:33:45 +0100

    put bot start into ready.js/ event handling

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 //prerequisites
+const fs = require('node:fs');
+const path = require('node:path');
 const { Client, Intents } = require('discord.js'); //gets discord.js
 const { token, clientId, guildId } = require('./config.json');  //gets token from config.json
 
 // Create a client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-//checks if client is ready to go
-client.once('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setPresence({ activities: [{ name: 'beep-boop, being worked on rn' }], status: 'online' }); //sets activity/status
-});
+//event handling
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
 
 // Login to Discord with your client's token
 client.login(token);
