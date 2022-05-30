5a7087511f91e5018e597aa9486025370d3b1ac2
Billy <billyzhaodong@gmail.com>
2022-05-30 17:00:19 +0100

    attempted command handling and failed lol

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 //prerequisites
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Intents } = require('discord.js'); //gets discord.js
+const { Client, Collection, Intents } = require('discord.js'); //gets discord.js
 const { token, clientId, guildId } = require('./config.json');  //gets token from config.json
 
 // Create a client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+//command handling
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    client.commands.set(command.data.name, command);
+}
 
 //event handling
 const eventsPath = path.join(__dirname, 'events');
